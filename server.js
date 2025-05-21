de09ddd7473f5d8e370ad9a1d98d4d516d1a5112
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-05-21 06:43:57 +0000

    Add basic routing to Node.js server with home and about pages

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-//include Express
+//include express 
 const express = require('express');
 
 //set port
 const port = 3000;
-
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send(`
-	    <h1>Here's my home page</h1>
-      <p>Welcome to Express!</p>
-  `);
-
-});
+	res.send(`
+	<h1>Here's my home page</h1>
+	<p>Welcome to Express!</p>
+`);
+})
 
 app.get('/about',(req,res)=>{
-    res.send(`
-	    <h1>Here's Bill's Express About Page</h1>
-      <p>Welcome to Express!</p>
-  `);
-
-});
-
+	res.send(`
+	<h1>This is Victor's Express about page</h1>
+	<p>Welcome to Express!</p>
+`);
+})
 
 //Set server to listen for requests
 app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+  console.log(`Server running at port: ${port}/`);
 });
